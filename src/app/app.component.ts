72ae50850a187ecf3a07463d099d6852b582e3f6
Ismael Perez <ismaelperezemail@gmail.com>
2020-04-16 20:17:48 -0700

    chore(App): password generate logic complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  useLetters = false;
+  useSymbols = false;
+  useNumbers = false;
+  passwordLength = 0;
 
   onButtonClick() {
-    this.password = 'My Password';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyx';
+    const symbols = '!@#$%^&*';
+
+    let validChars = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue;
+    }
   }
 }
